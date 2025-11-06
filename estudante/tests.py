16e1c9814679a9204bb4066c7eb3124a76070e67
sophia <16734314+SophiaBetoni@users.noreply.github.com>
2025-11-06 19:53:23 -0300

    teste de modelo do back do estudante

--- estudante/tests.py
+++ estudante/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from .models import Estudante
+
+class EstudanteModelTest(TestCase):
+    def test_criacao_estudante(self):
+        estudante = Estudante.objects.create(
+            nome='Sophia Betoni',
+            email='sophiabetoni@outlook.com'
+        )
+        self.assertEqual(estudante.nome, 'Sophia Betoni')
+        self.assertEqual(estudante.email, 'sophiabetoni@outlook.com')
+        self.assertEqual(str(estudante), 'Sophia Betoni')
